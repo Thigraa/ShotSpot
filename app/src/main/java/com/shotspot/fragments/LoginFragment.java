6705324a7410e07b42668ca41e16dc7d55114fc3
Joan <joan.lecha.7e3@itb.cat>
2021-05-06 19:08:52 +0200

    Layout Changes
    Created Welcome Fragment, made layout
    Created Login Fragment, made layout
    Created Register Fragment, made layout
    Created attrs.xml file to set responsive colors
    Created font values folder
    Added Raleway font family and Barlow Condensed font family
    Added 2 creative commons images to use in the welcome screen
    Modified Colors in themes

---
+++ app/src/main/java/com/shotspot/fragments/LoginFragment.java
@@ -0,0 +1,33 @@
+package com.shotspot.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shotspot.R;
+
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        return v;
+    }
+}
