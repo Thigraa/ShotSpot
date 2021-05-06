4bea6fcdaa1365f818524bb19036c8c88ba413a1
larios-gin <56580508+larios-gin@users.noreply.github.com>
2021-05-06 17:00:35 +0200

    sql consulta

--- app/src/main/java/com/shotspot/MainActivity.java
+++ app/src/main/java/com/shotspot/MainActivity.java
@@ -36,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Connection  connection = DatabaseConnection.connect();
+        try{
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM dbo.PERSON");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                System.out.println(rs.getString(2));
+            }
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
         imageView = findViewById(R.id.imagen);
         button = findViewById(R.id.botonAdd);
