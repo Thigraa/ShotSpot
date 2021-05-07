e69b23548ba0cfd9604eeb6416104ba767e6c4ba
Joan <joan.lecha.7e3@itb.cat>
2021-05-07 18:13:30 +0200

    Layout changes
    - Created menu of bottom navigation
    - Created Bottom Navigation
    - Created Home Fragment - layout in progress
    - Added a fragment to main activity layout
    - Included bottom navigation to main activity
    - Added implementations: Circle Image View and Carousel View

---
+++ app/src/main/java/com/shotspot/fragments/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.shotspot.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shotspot.R;
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+    }
+}
