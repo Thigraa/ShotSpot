017dcdddc6972d3a23aaca78c7daa9f3a5af352b
larios-gin <73183443+pauITB@users.noreply.github.com>
2021-05-10 19:09:50 +0400

    Starting ProfileFragment

---
+++ app/src/main/java/com/shotspot/fragments/ProfileFragment.java
@@ -0,0 +1,31 @@
+package com.shotspot.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shotspot.R;
+
+
+public class ProfileFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return v;
+    }
+}
