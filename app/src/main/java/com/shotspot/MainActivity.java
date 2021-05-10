1a7655d9a1362024789a4b1f0e99ea17ea299a9a
Joan <joan.lecha.7e3@itb.cat>
2021-05-10 19:09:52 +0200

    Register changes
    Now you can register
    Controlled neither Username repeat nor email

--- app/src/main/java/com/shotspot/MainActivity.java
+++ app/src/main/java/com/shotspot/MainActivity.java
@@ -26,6 +26,7 @@ import java.sql.SQLException;
 public class MainActivity extends AppCompatActivity {
     public static BottomNavigationView bottomNavigationView;
     public static Connection connection;
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +36,10 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setVisibility(View.INVISIBLE);
         connection = DatabaseConnection.connect();
         Fragment fragment;
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             fragment = getSupportFragmentManager().getFragment(savedInstanceState, "lastFragment");
             replaceFragment(fragment);
-        }
-        else{
+        } else {
             fragment = new WelcomeFragment();
             replaceFragment(fragment);
 
@@ -48,90 +48,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public void replaceFragment(Fragment f){
+    public void replaceFragment(Fragment f) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.navHost, f, f.getClass().getSimpleName()).addToBackStack(null)
                 .commit();
     }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        for(int i = 0; i< getSupportFragmentManager().getFragments().size(); i++ ){
-            if(getSupportFragmentManager().getFragments().get(i).isVisible()){
-                Fragment f =  getSupportFragmentManager().getFragments().get(i);
+        for (int i = 0; i < getSupportFragmentManager().getFragments().size(); i++) {
+            if (getSupportFragmentManager().getFragments().get(i).isVisible()) {
+                Fragment f = getSupportFragmentManager().getFragments().get(i);
                 getSupportFragmentManager().putFragment(savedInstanceState, "lastFragment", f);
             }
-        }
-
-        imageView = findViewById(R.id.imagen);
-        button = findViewById(R.id.botonAdd);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SelectImageFromGallery();
-            }
-        });
-
-        uploadImageButton =  findViewById(R.id.botonUpload);
-        uploadImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UploadImage();
-                Intent intent = new Intent(MainActivity.this,PruebaActivity.class);
-                startActivity(intent);
-            }
-        });
-        uploadImageButton.setEnabled(false);
-
-
-    }
-
-    private void SelectImageFromGallery()
-    {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
-    }
-
-    private void UploadImage()
-    {
-        try {
-            final InputStream imageStream = getContentResolver().openInputStream(this.imageUri);
-            final int imageLength = imageStream.available();
-
-                    try {
-                        final String imageName = ImageManager.UploadImage(imageStream, imageLength);
-                                Toast.makeText(MainActivity.this, "Image Uploaded Successfully. Name = " + imageName, Toast.LENGTH_SHORT).show();
-
-                    }
-                    catch(Exception ex) {
-                        final String exceptionMessage = ex.getMessage();
-                        Toast.makeText(MainActivity.this, exceptionMessage, Toast.LENGTH_SHORT).show();
-                        ex.printStackTrace();
-
-
-                    }
-        }
-        catch(Exception ex) {
-
-            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-
-        switch (requestCode) {
-            case SELECT_IMAGE:
-                if (resultCode == RESULT_OK) {
-                    this.imageUri = imageReturnedIntent.getData();
-                    this.imageView.setImageURI(this.imageUri);
-                    this.uploadImageButton.setEnabled(true);
-                }
         }
     }
 }
--- app/src/main/java/com/shotspot/fragments/RegisterFragment.java
+++ app/src/main/java/com/shotspot/fragments/RegisterFragment.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import com.shotspot.MainActivity;
 import com.shotspot.R;
 import com.shotspot.database.DatabaseConnection;
+import com.shotspot.database.Person_CRUD;
 import com.shotspot.helper.Encryptor;
 import com.shotspot.model.Person;
 
@@ -61,59 +62,81 @@ public class RegisterFragment extends Fragment {
            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
            @Override
            public void onClick(View v) {
-               boolean usernameRepeat = false;
-               boolean emailRepeat= false;
-               if(!username.getText().toString().isEmpty() && !email.getText().toString().isEmpty() && !password.getText().toString().isEmpty() && !repeatPassword.getText().toString().isEmpty()){
-                   if(password.getText().toString().length() >= 8){
-                       if(password.getText().toString().equals(repeatPassword.getText().toString())){
-                           if(checkBox.isChecked()){
-                               if(email.getText().toString().trim().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")){
-                                   try {
-                                       progressBar.setVisibility(View.VISIBLE);
-                                       register.setClickable(false);
-                                       register.setFocusable(false);
-                                       PreparedStatement preparedStatement = DatabaseConnection.connect().prepareStatement("SELECT * FROM dbo.PERSON WHERE USERNAME = ?");
-                                       preparedStatement.setString(1, username.getText().toString());
-                                       ResultSet resultSet = preparedStatement.executeQuery();
-                                       if(resultSet.next()) {
-                                           usernameRepeat = true;
-                                       }
-                                       PreparedStatement preparedStatement1 = DatabaseConnection.connect().prepareStatement("SELECT * FROM dbo.PERSON WHERE USERNAME = ?");
-                                       preparedStatement1.setString(1, email.getText().toString());
-                                       ResultSet resultSet1 = preparedStatement1.executeQuery();
-                                       if(resultSet1.next()){
-                                           emailRepeat = true;
-                                       }
-                                   } catch (SQLException throwables) {
-                                       throwables.printStackTrace();
-                                   }
-                                   if(!usernameRepeat){
-                                       if(!emailRepeat){
-                                           String passwordEncrypted = Encryptor.md5(password.getText().toString());
-                                           Person person = new Person(username.getText().toString(), email.getText().toString(), passwordEncrypted, null);
-                                       }else{
-                                           Toast.makeText(getContext(), "Email in use", Toast.LENGTH_SHORT).show();
-                                       }
-                                   }else{
-                                       Toast.makeText(getContext(), "Username in use", Toast.LENGTH_SHORT).show();
-                                   }
-                               }else{
-                                   Toast.makeText(getContext(), "Please type a valid format email", Toast.LENGTH_SHORT).show();
-                               }
-                           }else{
-                               Toast.makeText(getContext(), "Terms and Conditions must be accepted", Toast.LENGTH_SHORT).show();
-                           }
-                       }else{
-                           Toast.makeText(getContext(), "Passwords must match", Toast.LENGTH_SHORT).show();
-                       }
-                   }else{
-                       Toast.makeText(getContext(), "Password must contain minimum 8 characters", Toast.LENGTH_SHORT).show();
-                   }
-               }else{
-                   Toast.makeText(getContext(), "Please fill all the fields", Toast.LENGTH_SHORT).show();
-               }
+              registerManager();
            }
        });
         return v;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void registerManager(){
+        boolean usernameRepeat = false;
+        boolean emailRepeat= false;
+        if(!username.getText().toString().isEmpty() && !email.getText().toString().isEmpty() && !password.getText().toString().isEmpty() && !repeatPassword.getText().toString().isEmpty()){
+            if(password.getText().toString().length() >= 8){
+                if(password.getText().toString().equals(repeatPassword.getText().toString())){
+                    if(checkBox.isChecked()){
+                        if(email.getText().toString().trim().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")){
+                            try {
+                                progressBar.setVisibility(View.VISIBLE);
+                                register.setClickable(false);
+                                register.setFocusable(false);
+                                PreparedStatement preparedStatement = DatabaseConnection.connect().prepareStatement("SELECT * FROM dbo.PERSON WHERE USERNAME = ?");
+                                preparedStatement.setString(1, username.getText().toString());
+                                ResultSet resultSet = preparedStatement.executeQuery();
+                                if(resultSet.next()) {
+                                    usernameRepeat = true;
+                                }
+                                PreparedStatement preparedStatement1 = DatabaseConnection.connect().prepareStatement("SELECT * FROM dbo.PERSON WHERE EMAIL = ?");
+                                preparedStatement1.setString(1, email.getText().toString());
+                                ResultSet resultSet1 = preparedStatement1.executeQuery();
+                                if(resultSet1.next()){
+                                    emailRepeat = true;
+                                }
+                            } catch (SQLException throwables) {
+                                throwables.printStackTrace();
+                            }
+                            if(!usernameRepeat){
+                                if(!emailRepeat){
+                                    String passwordEncrypted = Encryptor.md5(password.getText().toString());
+                                    Person person = new Person(username.getText().toString(), email.getText().toString(), passwordEncrypted, null);
+                                    Person_CRUD.insert(person);
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                    register.setFocusable(true);
+                                    register.setClickable(true);
+                                    replaceFragment(new LoginFragment());
+
+                                }else{
+                                    Toast.makeText(getContext(), "Email in use", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                    register.setFocusable(true);
+                                    register.setClickable(true);
+                                }
+                            }else{
+                                Toast.makeText(getContext(), "Username in use", Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                                register.setFocusable(true);
+                                register.setClickable(true);
+                            }
+                        }else{
+                            Toast.makeText(getContext(), "Please type a valid format email", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getContext(), "Terms and Conditions must be accepted", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(getContext(), "Passwords must match", Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(getContext(), "Password must contain minimum 8 characters", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(getContext(), "Please fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void replaceFragment(Fragment f) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.navHost, f, f.getClass().getSimpleName()).addToBackStack(null)
+                .commit();
     }
 }
