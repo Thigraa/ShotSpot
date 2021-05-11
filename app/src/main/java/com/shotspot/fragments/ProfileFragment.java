5dd8f76114e18be1e779747aa92b1f84fa9169b1
pauITB <pau.gisbert.7e3@itb.cat>
2021-05-11 19:15:53 +0200

    Person_CRUD added to search by username and password and updates SpotImage_CRUD change names ImageManager set delete method ProfileFragment modified and Adapter modified

--- app/src/main/java/com/shotspot/fragments/ProfileFragment.java
+++ app/src/main/java/com/shotspot/fragments/ProfileFragment.java
@@ -7,13 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.shotspot.R;
+import com.shotspot.database.Person_CRUD;
+import com.shotspot.database.SpotImage_CRUD;
+import com.shotspot.model.Person;
+import com.shotspot.storage.ImageManager;
+
+import java.io.ByteArrayOutputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class ProfileFragment extends Fragment {
 
+    CircleImageView profileImageView;
+    TextView usernameTV;
 
     BottomNavigationView profileSpotsNavigation;
     @Override
@@ -27,6 +40,16 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        //TODO Change to use current user
+        Person user = Person_CRUD.getPerson(4);
+        usernameTV = v.findViewById(R.id.profileUsernameTextView);
+        profileImageView = v.findViewById(R.id.profileUserCircleImage);
+        usernameTV.setText(user.getUsername());
+        try {
+            profileImageView.setImageBitmap(user.getBitmap());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Fragment fragment = new MySpotsFragment();
         replaceFragment(fragment);
         return v;
@@ -37,4 +60,5 @@ public class ProfileFragment extends Fragment {
                 .replace(R.id.profileSpots, f, f.getClass().getSimpleName()).addToBackStack(null)
                 .commit();
     }
+
 }
