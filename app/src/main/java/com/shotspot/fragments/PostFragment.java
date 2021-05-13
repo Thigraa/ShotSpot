440549f4cb055a63193fd31f057b3a8678befa35
Joan <joan.lecha.7e3@itb.cat>
2021-05-13 19:08:08 +0200

    Post Fragment
    Started post fragment
    Started to create the tags method

---
+++ app/src/main/java/com/shotspot/fragments/PostFragment.java
@@ -0,0 +1,36 @@
+package com.shotspot.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shotspot.R;
+
+
+public class PostFragment extends Fragment {
+
+
+    public PostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_post, container, false);
+
+        return v;
+    }
+}
